4b6e2353bffc274aa786dab8ed278033087e8533
Bibek <www.koiralabibek2058@gmail.com>
2025-12-25 21:43:08 +0545

    project intialize

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('MONGO_URI');
+        if (!uri) {
+          throw new Error(
+            'MONGO_URI is not defined in the environment variables',
+          );
+        }
+        return {
+          uri,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ server/src/users/users.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) { }
+
+}
